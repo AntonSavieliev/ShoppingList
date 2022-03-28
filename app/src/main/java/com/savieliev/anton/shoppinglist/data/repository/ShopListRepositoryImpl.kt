21e73a9b7ee880e3d7892bea0fa27fddf45a2bf7
Anton Savieliev <ant.savelev@gmail.com>
2022-03-28 15:52:04 +0300

    adding temporary repository implementation

---
+++ app/src/main/java/com/savieliev/anton/shoppinglist/data/repository/ShopListRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.savieliev.anton.shoppinglist.data.repository
+
+import com.savieliev.anton.shoppinglist.domain.entities.ShopItem
+import com.savieliev.anton.shoppinglist.domain.repository.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
