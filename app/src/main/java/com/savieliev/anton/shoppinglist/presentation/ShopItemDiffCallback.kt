5f347b0fce07f3a1ed8aca2568b526f358661e64
Anton Savieliev <ant.savelev@gmail.com>
2022-03-30 15:52:50 +0300

    adding RecyclerView

---
+++ app/src/main/java/com/savieliev/anton/shoppinglist/presentation/ShopItemDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.savieliev.anton.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.savieliev.anton.shoppinglist.domain.entities.ShopItem
+
+class ShopItemDiffCallback : DiffUtil.ItemCallback<ShopItem>() {
+
+    override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem == newItem
+    }
+}
